c5091e80f008eb80cd8e0da1fcff8ef3cc52a755
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-05-30 11:57:27 +0100

    Sort by primitive and non primitive data types

--- Java-Basics-1/Java-Basics-1/Java-Basics-1/src/com/NotKatsu/test/Main.java
+++ Java-Basics-1/Java-Basics-1/Java-Basics-1/src/com/NotKatsu/test/Main.java
@@ -2,17 +2,16 @@ package com.NotKatsu.test;
 
 public class Main {
     public static void main(String[] args) {
-        String blank_sentence;
-        String sentence = "This is my sentence";
-
+        // PRIMITIVE
         int number = 5;
-
         double decimal_number = 104.2;
-
         boolean true_boolean = true;
         boolean false_boolean = false;
-
         char letter = 'a';
+        
+        // NON-PRIMITIVE
+        String blank_sentence;
+        String sentence = "This is my sentence";
 
         System.out.println(sentence);
     }
