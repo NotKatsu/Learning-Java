85108f9f731ea63e3ea07fa8335c5c0744af67ce
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-05-30 12:42:33 +0100

    Basic if statement to check if number var is bigger than 3

--- Java-Basics-2/src/com/NotKatsu/test/Main.java
+++ Java-Basics-2/src/com/NotKatsu/test/Main.java
@@ -2,6 +2,12 @@ package com.NotKatsu.test;
 
 public class Main {
     public static void main(String[] args) {
+        int number = 2;
 
+        if (number > 3) {
+            System.out.println("Number is greater than 3!");
+        } else {
+            System.out.println("Number is less than 3!");
+        }
     }
 }
