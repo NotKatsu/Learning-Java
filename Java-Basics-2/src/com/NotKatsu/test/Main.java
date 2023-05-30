016d0d93577337fc3a0229e3bf4af3d170aa2bd9
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-05-30 13:15:22 +0100

    Comment out old code

--- Java-Basics-2/src/com/NotKatsu/test/Main.java
+++ Java-Basics-2/src/com/NotKatsu/test/Main.java
@@ -2,23 +2,23 @@ package com.NotKatsu.test;
 
 public class Main {
     public static void main(String[] args) {
-        String message = null;
-
-        try {
-            boolean contains_result = check_if_contains(message);
-
-            if (contains_result) {
-                System.out.println("message contains the word hello or world!");
-            } else {
-                System.out.println("message does not contain the word hello or world!");
-            }
-        } catch (NullPointerException e) {
-            System.out.println("We got a NullPointerException!");
-            // e.printStackTrace();
-        }
+//        String message = null;
+//
+//        try {
+//            boolean contains_result = check_if_contains(message);
+//
+//            if (contains_result) {
+//                System.out.println("message contains the word hello or world!");
+//            } else {
+//                System.out.println("message does not contain the word hello or world!");
+//            }
+//        } catch (NullPointerException e) {
+//            System.out.println("We got a NullPointerException!");
+//            // e.printStackTrace();
+//        }
 
     }
-    public static boolean check_if_contains(String message) {
-        return message.toLowerCase().contains("world") || message.toLowerCase().contains("hello");
-    }
+//    public static boolean check_if_contains(String message) {
+//        return message.toLowerCase().contains("world") || message.toLowerCase().contains("hello");
+//    }
 }
