98485502639c4481decb4a526f1cfc1d640cae99
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-05-30 16:18:45 +0100

    Comment old code out

--- Java-Basics-3/src/com/NotKatsu/test/Main.java
+++ Java-Basics-3/src/com/NotKatsu/test/Main.java
@@ -2,15 +2,15 @@ package com.NotKatsu.test;
 
 public class Main {
     public static void main(String[] args) {
-        String[] array = new String[]{"this", "is", "my", "array"};
-
-        for (int count = 0; count < array.length; count++) {
-            System.out.println(array[count]);
-        }
-
-        for (String string : array) {
-            System.out.println(string);
-        }
+//        String[] array = new String[]{"this", "is", "my", "array"};
+//
+//        for (int count = 0; count < array.length; count++) {
+//            System.out.println(array[count]);
+//        }
+//
+//        for (String string : array) {
+//            System.out.println(string);
+//        }
 
     }
 }
