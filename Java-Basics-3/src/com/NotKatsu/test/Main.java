4719c0c19a62a10f167585c6e4fe1492db15e7df
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-05-30 13:33:02 +0100

    Create basic array

--- Java-Basics-3/src/com/NotKatsu/test/Main.java
+++ Java-Basics-3/src/com/NotKatsu/test/Main.java
@@ -2,6 +2,6 @@ package com.NotKatsu.test;
 
 public class Main {
     public static void main(String[] args) {
-        
+        String[] array = new String[]{"this", "is", "my", "array"};
     }
 }
