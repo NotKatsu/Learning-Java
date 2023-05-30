7694c2898ed7dcd4a4149bbfa1c6d4f1895246a7
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-05-30 11:49:33 +0100

    Create 2 variables, blank_sentence and sentence

--- Java-Basics-1/Java-Basics-1/Java-Basics-1/src/com/NotKatsu/test/Main.java
+++ Java-Basics-1/Java-Basics-1/Java-Basics-1/src/com/NotKatsu/test/Main.java
@@ -2,6 +2,7 @@ package com.NotKatsu.test;
 
 public class Main {
     public static void main(String[] args) {
-        
+        String blank_sentence;
+        String sentence = "This is my sentence";
     }
 }
