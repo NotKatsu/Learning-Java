1186b7a5eb57ee79bb3535490b62cc737c8429aa
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-05-30 12:45:49 +0100

    Simplify code and don't check if true

--- Java-Basics-2/src/com/NotKatsu/test/Main.java
+++ Java-Basics-2/src/com/NotKatsu/test/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         String message = "HELLo WorLd!";
 
-        if (message.toLowerCase().contains("world") == true) {
+        if (message.toLowerCase().contains("world")) {
             System.out.println("message contains the word world!");
         } else {
             System.out.println("message does not contain the word world!");
