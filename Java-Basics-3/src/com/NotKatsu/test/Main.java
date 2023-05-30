0ae2e25b5a4ec6bb2b0c65b630f5424778770958
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-05-30 13:55:14 +0100

    Loop through all elements in an array

--- Java-Basics-3/src/com/NotKatsu/test/Main.java
+++ Java-Basics-3/src/com/NotKatsu/test/Main.java
@@ -3,5 +3,10 @@ package com.NotKatsu.test;
 public class Main {
     public static void main(String[] args) {
         String[] array = new String[]{"this", "is", "my", "array"};
+
+        for (int count = 0; count < array.length; count++) {
+            System.out.println(array[count]);
+        }
+
     }
 }
