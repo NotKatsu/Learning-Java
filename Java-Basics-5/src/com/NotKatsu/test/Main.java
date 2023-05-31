e8f7adb81edbd9eb1d8e2a18345e8610eb8a00e5
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-05-31 18:56:16 +0100

    Comment out old code again

--- Java-Basics-5/src/com/NotKatsu/test/Main.java
+++ Java-Basics-5/src/com/NotKatsu/test/Main.java
@@ -1,7 +1,5 @@
 package com.NotKatsu.test;
 
-import java.util.Calendar;
-
 public class Main {
     public static void main(String[] args) {
 //        String word = "banana";
@@ -26,10 +24,10 @@ public class Main {
 //                System.out.println("At least give me a fruit..");
 //        }
 
-        Calendar calendar = Calendar.getInstance();
-        System.out.println(calendar.get(Calendar.DAY_OF_MONTH));
-
-        calendar.add(Calendar.DAY_OF_YEAR, 1);
-        System.out.println(calendar.get(Calendar.DAY_OF_MONTH));
+//        Calendar calendar = Calendar.getInstance();
+//        System.out.println(calendar.get(Calendar.DAY_OF_MONTH));
+//
+//        calendar.add(Calendar.DAY_OF_YEAR, 1);
+//        System.out.println(calendar.get(Calendar.DAY_OF_MONTH));
     }
 }
