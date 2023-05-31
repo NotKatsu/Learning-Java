d041caeb6c94feac036fb3d26ab8a2b352ea1d38
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-05-31 18:48:01 +0100

    Comment out old code

--- Java-Basics-5/src/com/NotKatsu/test/Main.java
+++ Java-Basics-5/src/com/NotKatsu/test/Main.java
@@ -2,26 +2,26 @@ package com.NotKatsu.test;
 
 public class Main {
     public static void main(String[] args) {
-        String word = "bean";
-
-        if (word.equals("banana")) {
-            System.out.println("Word is banana!");
-        } else if (word.equals("pear")) {
-            System.out.println("Eww Pears are bad!!");
-        }
-
-        switch (word) {
-            case "banana":
-                System.out.println("Wow banana");
-                break;
-            case "pear":
-                System.out.println("Ew pear");
-                break;
-            case "kiwi":
-                System.out.println("These are kinda nice...");
-                break;
-            default:
-                System.out.println("At least give me a fruit..");
-        }
+//        String word = "banana";
+//
+//        if (word.equals("banana")) {
+//            System.out.println("Word is banana!");
+//        } else if (word.equals("pear")) {
+//            System.out.println("Eww Pears are bad!!");
+//        }
+//
+//        switch (word) {
+//            case "banana":
+//                System.out.println("Wow banana");
+//                break;
+//            case "pear":
+//                System.out.println("Ew pear");
+//                break;
+//            case "kiwi":
+//                System.out.println("These are kinda nice...");
+//                break;
+//            default:
+//                System.out.println("At least give me a fruit..");
+//        }
     }
 }
