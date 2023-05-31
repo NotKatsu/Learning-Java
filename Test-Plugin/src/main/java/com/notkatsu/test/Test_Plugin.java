b91bf32a73f9a3c061c61d1ac0692dc4c1f6043b
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-05-31 20:03:43 +0100

    Generate jars

--- Test-Plugin/src/main/java/com/notkatsu/test/Test_Plugin.java
+++ Test-Plugin/src/main/java/com/notkatsu/test/Test_Plugin.java
@@ -7,11 +7,12 @@ public final class Test_Plugin extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        System.out.println("Plugin has started!!");
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+        System.out.println("Plugin has stopped.. :(");
     }
 }
